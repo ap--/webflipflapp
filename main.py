b1fef856e2081bf2e45ab62fe50524fdcb886e53
ap-- <mail@andreaspoehlmann.de>
2013-11-21 16:44:20 +0100

    removed sys.path 3rd party

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib_third_party'))
 
 
 ### WebServer
